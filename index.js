3b1fb9a20f5b89c5cefca68a0abb2a3b9601ea2d
Archie Baer <archie@abaer.dev>
2021-08-31 20:12:41 +0100

    Express server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 require("dotenv").config();
+const { PORT } = process.env;
+
+const express = require("express");
+const app = express();
+app.set("trust proxy", 1);
+app.use(require("body-parser").json());
+app.listen(PORT || 8080, () => console.log("Server is listening"));
+
+// Error
+app.use((_req, res) => res.status(404).send("Not Found"));
+app.use((_err, _req, res, _next) => res.status(500).send("Internal Error"));
