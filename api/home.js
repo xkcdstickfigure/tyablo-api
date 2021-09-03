43bc17ab938b05336117f31f4e66dc6a478d5dc6
Archie Baer <archie@abaer.dev>
2021-09-03 21:44:08 +0100

    Formats phone number in homepage data

--- api/home.js
+++ api/home.js
@@ -1,9 +1,11 @@
+const { intlFormat } = require("../util/phoneNumber");
+
 module.exports = (req, res) => {
   const { user } = req.session;
   res.json({
     id: user.id,
     name: user.name,
-    phone: user.phone,
+    phone: intlFormat(user.phone),
     score: user.score,
   });
 };
