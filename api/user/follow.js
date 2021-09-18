9486b14dd75297e76f5807a604857f97125c6390
Archie Baer <archie@abaer.dev>
2021-09-18 10:41:12 +0100

    Prevents self follow

--- api/user/follow.js
+++ api/user/follow.js
@@ -8,6 +8,10 @@ module.exports = async (req, res) => {
   const user2 = await db.user.findUnique({ where: { id } });
   if (!user2) return res.status(400).send("Missing Resource");
 
+  // Prevent Self Follow
+  if (user.id === user2.id)
+    return res.status(403).send("Self Follow Not Allowed");
+
   // Create Follow
   await db.follow.upsert({
     where: {
