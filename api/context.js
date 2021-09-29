9789ec5cf2b46957c7377fa20383a81625411e92
Archie Baer <archie@abaer.dev>
2021-09-29 22:47:35 +0100

    Context endpoint with managed pages list

---
+++ api/context.js
@@ -0,0 +1,33 @@
+const { ORIGIN } = process.env;
+const db = require("../prisma");
+const { intlFormat } = require("../util/phoneNumber");
+
+module.exports = async (req, res) => {
+  const { user } = req.session;
+
+  const pages = await db.page.findMany({
+    where: {
+      managers: {
+        some: {
+          userId: user.id,
+        },
+      },
+    },
+  });
+
+  res.json({
+    user: {
+      id: user.id,
+      name: user.name,
+      avatar: user.avatar
+        ? `${ORIGIN}/fs/${user.avatar}`
+        : `${ORIGIN}/s/uavatar.png`,
+      phone: intlFormat(user.phone),
+      score: user.score,
+    },
+    pages: pages.map((p) => ({
+      id: p.id,
+      name: p.name,
+    })),
+  });
+};
--- api/index.js
+++ api/index.js
@@ -8,14 +8,14 @@ const auth = require("./auth");
 api.post("/login/phone", require("./login/phone"));
 api.post("/login/code", require("./login/code"));
 
+// Context
+api.get("/context", auth, require("./context"));
+
 // Set Location
 api.post("/location", auth, require("./location"));
 
 // Account
 api.post("/account", auth, require("./account/update"));
-
-// Home
-api.get("/home", auth, require("./home"));
 
 // Feed
 api.get("/feed", auth, require("./feed"));
